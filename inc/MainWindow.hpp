ff2772c4e1df94fbc8b02c6f5b78a7fad343e43a
ChenL <liuyuchen6556@163.com>
2023-07-01 07:11:40 +0800

    多线程优化、加入缓存机制，增加速度调节选项，增加随机填充功能。

--- inc/MainWindow.hpp
+++ inc/MainWindow.hpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <QMouseEvent>
 #include <QLabel>
+#include <deque>
+#include <mutex>
+#include <thread>
 
 
 QT_BEGIN_NAMESPACE
@@ -20,6 +23,10 @@ private:
     bool _paintable = true;
     int _paint = 0; // 0:None 1:Paint 2:Erase
     std::vector<std::vector<bool>> _checkerboard;
+    bool _refresh_cache = false;
+    std::deque<std::vector<std::vector<bool>>> _cache;
+    std::mutex _mutex;
+    std::thread *_refresh_thread = nullptr;
 
     QTimer _timer;
     size_t _count = 0;
@@ -30,6 +37,10 @@ private:
     void init();
 
     bool eventFilter(QObject *obj, QEvent *event);
+
+    void refresh_cache_subfunc(std::tuple<std::vector<std::vector<bool>> *, const int, const int> args);
+
+    void refresh_cache();
 
 private slots:
     void play();
@@ -44,7 +55,9 @@ private slots:
 
     void write();
 
-    void change_speed();
+    void change_speed(const int value);
+
+    void fill();
 
 protected:
     void mousePressEvent(QMouseEvent *event);
